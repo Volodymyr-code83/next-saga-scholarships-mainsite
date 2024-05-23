bf653f0d9799b378f4d25b37b236e742e00a87f3
POT <webdev539@gmail.com>
2024-05-23 14:13:07 +0300

    relast

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1fw7k9LAoTITNoCHyUSiPCc8XSpcP32Yu2k+YsyMZzY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FZtH3/V5GYv6CGHfiVgK8VCrrhk3r/SY4ENz/Z7OhzI=\"\n}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3a6eda76b267e339"; }
+/******/ 		__webpack_require__.h = function() { return "8e37a66358494e62"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
